4dcb1bfac2c2d197764b030e1ecdf44e75cf5d6d
pipo411 <35770134+pipo411@users.noreply.github.com>
2018-08-16 15:35:38 -0400

    acceptance

---
+++ acceptance/src/test/java/gradle/cucumber/BasicSteps.java
@@ -0,0 +1,26 @@
+package gradle.cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.PendingException;
+
+public class BasicSteps {
+    @Given("A failing scenario")
+    public void a_failing_scenario() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+        }
+
+    @When("I run a failing step")
+    public void i_run_a_failing_step() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+        }
+
+    @Then("I got a failing step")
+    public void i_got_a_failing_step() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+        }
+}
